6f4fb7529471c787ac5cf1f6ac53d0ce0fdace62
kurtamodia-opswerks <km.amodia@opswerks.com>
2025-11-13 11:15:13 +0800

    feat: sync logged in user from microsoft entra id with local mongodb database

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -9,4 +9,24 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       issuer: `https://login.microsoftonline.com/${process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID}/v2.0`,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/sync`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: user.email,
+            name: user.name,
+            image: user.image,
+          }),
+        });
+        console.log("user is synced with local db.");
+      } catch (error) {
+        console.error("Failed to sync user with backend", error);
+      }
+
+      return true;
+    },
+  },
 });
